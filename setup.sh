f963d7a4291d9f67f18ab6ce392081e4375bffe3
Bin Zheng (bizheng) <bizheng@cisco.com>
2018-03-12 21:16:28 +0800

    Add tmux manager plugins

--- setup.sh
+++ setup.sh
@@ -13,3 +13,7 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # install tig, a text mode git ui
 #yum install tig
+
+# install tmux plugin manager, require tmux >= 1.9
+#git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
